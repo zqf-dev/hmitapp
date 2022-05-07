3381bd8f6b6ae129b17ab09311661faa7a487b23
zhangqingfa <zhang.qingfa@foundbyte.com>
2022-05-07 09:59:58 +0800

    增加注释

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <!-- 评论列表 -->
+  <!-- 1. van-list最好在这个div内, 让底部文字/加载中, 作为此div内容, 容器padding-bottom
+    底部文字也挤上去了-看得见
+     -->
+  <div>
+
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,4 @@ export default {
 </script>
 
 <style>
-
 </style>
